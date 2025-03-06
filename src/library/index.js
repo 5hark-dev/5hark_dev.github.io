079f1024840fca034558de7a60f5e878c7b9c9de
Justin <57233583+5hark-dev@users.noreply.github.com>
2025-03-06 11:08:33 -0500

    added renderer to index export

--- src/library/index.js
+++ src/library/index.js
@@ -3,12 +3,13 @@ import Keycodes from "./controls/keycodes.js";
 import MouseControls from "./controls/mousecontrols.js";
 import Vec2 from "./vec2.js";
 import Container from "./container.js";
-
+import CanvasRenderer from "./canvasrenderer.js"
 export default 
 {
     KeyControls,
     Keycodes,
     MouseControls,
     Vec2,
-    Container
+    Container,
+    CanvasRenderer
 };
